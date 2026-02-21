c28a3bd07e247c6996afae8169489f64405dc2a2
MINDEV\kin41 <kin412@naver.com>
2026-02-21 22:28:19 +0900

    연관관계 매핑 기초 - 양방향 연관관계

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -33,25 +35,53 @@ public class JpaMain {
             Long findTeamId = findMember.getTeamId();
             Team findTeam = em.find(Team.class, findTeamId);*/
 
-            //member에 연관관예 manyToOne을 사용한 후
+            //member에 연관관계 manyToOne을 사용한 후
             // 이런식으로 그냥 team을 가져올수있음.
             //위에서 setName을 했고, id의 경우 시퀀스 전략으로 가져왔기 때문에
             // team객체에는 id, name이 존재
             member.setTeam(team);
 
+            //member를 1차캐시에 저장 및 쓰기 지연 저장소에 쿼리생성
             em.persist(member);
 
             //아래 find 쿼리를 직접 보고싶을 경우 일단 flush를 날린 후
             //영속성 컨텍스트를 비워서 1차캐시를 비운다.
             //그럼 find 시 1차캐시가 비워져있기때문에
-            //db까지가서 쿼리로 조회 후 가져오기때문에 쿼리가 보인다.
+            //db까지가서 쿼리로 조회 후 가져오기때문에 아래 find 쿼리가 보인다.
+            // clear하지 않는다면 아래의 findmember는 그냥 위에서 em.persist(member);
+            //한 member가 1차캐시에 저장되어 있으므로 db까지 가지않고 그냥 저 member를 반환
+
+            /*양방향의 경우
+            member.setTeam(team); 을 통해서 member의 team은 set함.
+            반대로 team의 List<Member> members 는 set하지 않았음.
+            아래 find쿼리는 member가 1차 캐시에 있는 것을 보고 실행되지 않고
+            그냥 1차캐시에 있는 member를 return 함
+            db에서 데이터를 가져오지 않았고 team의 List<Member> members는
+            set하지 않았기 때문에 아래의 findMember.getTeam().getMembers();
+            이 구문은 member가 없어서 size가 0.
+            flush() 후 clear()하면 위에서 만든 team과 member가 저장된 후
+             1차 캐시와 쓰기지연 저장소가 비기때문에
+            아래의 find시 db에서 데이터를 가져오므로 연관관계가 매핑되어 size가 1
+            */
             /*em.flush();
             em.clear();*/
 
             Member findMember = em.find(Member.class, member.getId());
 
+            //단방향
             Team findTeam = findMember.getTeam(); // 이런식으로 그냥 team을 가져올수있음.
             System.out.println("findTeam = " + findTeam);
+            //
+
+            //양방향
+            List<Member> members = findMember.getTeam().getMembers();
+
+            System.out.println("members.size() = " + members.size());
+
+            for (Member m : members) {
+                System.out.println("Member m = " + m.getUsername());
+            }
+            //
 
             tx.commit();
         } catch (Exception e) {
--- ex1-hello-jpa/src/main/java/hellojpa/Team.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -1,9 +1,9 @@
 package hellojpa;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -12,6 +12,20 @@ public class Team {
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
+
+    //양방향 연관관계
+    // 이 "team"은 Member객체 내부의 private Team team; 이 team.
+    //mappedBy - 반대방향의 주인의 team 멤버변수에 매핑된다.
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<Member>();
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
 
     public Long getId() {
         return id;
