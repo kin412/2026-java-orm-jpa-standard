5e3ba7b6e4af0315ba96745316c248d1d0b355a8
MINDEV\kin41 <kin412@naver.com>
2026-02-21 18:29:12 +0900

    연관관계 매핑 기초 - 단방향 연관관계

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -14,51 +14,39 @@ public class JpaMain {
         tx.begin(); //트랜잭션 시작
 
         try {
-            /*
+
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
             Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB"); // 객체 생성부터 여기까지가 비영속 상태
+            member.setUsername("member1");
 
-            // 엔티티 매니저 안의 영속성 컨텍스트로 관리되는 영속 상태
+            //member1을 팀A에 소속시키고 싶다면
+            //em.persist(team); 이시점에 시퀀스전략으로 db에 붙어서 시퀀스만 가져옴. 저장은 안함
+            //근데 이게 객체 스럽지 못함. 아래의 긴과정으로 팀을 또 찾아야함.
+            /*member.setTeamId(team.getId());
             em.persist(member);
-            em.detach(member); // 회원 엔티티를 영속성 컨텍스트에서 분리, 준영속 상태
-            em.remove(member); 객체를 삭제한 상태
-            */
 
-            //jpql 예시 - 객체 대상 조회 Member가 테이블이 아니라 객체 Member임
-            //List<Member> result = em.createQuery("select m from Member", Member.class).getResultList();
+            Member findMember = em.find(Member.class, member.getId());
 
-            /*Member findMember = em.find(Member.class, 1L);
+            Long findTeamId = findMember.getTeamId();
+            Team findTeam = em.find(Team.class, findTeamId);*/
 
-            findMember.setName("HelloJPA");
+            //member에 연관관예 manyToOne을 사용한 후
+            member.setTeam(team); // 이런식으로 그냥 team을 가져올수있음.
 
-            //업데이트시에는 setName으로 변경해두면 변경된 채로 1차캐시에 두고
-            //트랜잭션 커밋시 엔티티와 스냅샷을 비교해 업데이트쿼리가 실행되므로 persist를 하지않는게 맞음
-            em.persist(findMember);*/
+            em.persist(member);
 
-            //영속성 컨텍스트에서 같은 트랜잭션 안이라면 동일성 보장.
-            //자바컬렉션에서 같은 객체를 꺼냈을때 유지되는 것처럼.
-            //Member findMember2 = em.find(Member.class, 1L);
-            //Member findMember3 = em.find(Member.class, 1L);
+            //아래 find 쿼리를 직접 보고싶을 경우 일단 flush를 날린 후 영속성 컨텍스트를 비워서 1차캐시를 비운다.
+            /*em.flush();
+            em.clear();*/
 
-            //영속성 컨텍스트는 최초 findMember2 조회시 member, 1L 조건에 대한 데이터를 영속성 컨텍스트안의
-            //1차캐시에 보관함. 그리고 findMember3 조회 시 그 값을 그대로 줌.
-            // 그렇기에 db에서 불러오는 쿼리로그는 한번만 찍히고 둘이 같은 객체를 가지게 되므로 true
-            //System.out.println(findMember2 == findMember3);
+            Member findMember = em.find(Member.class, member.getId());
 
-            //영속
-            /*MemberV1 memberV11 = new MemberV1(150L, "A");
-            MemberV1 memberV12 = new MemberV1(160L, "B");
+            Team findTeam = findMember.getTeam(); // 이런식으로 그냥 team을 가져올수있음.
+            System.out.println("findTeam = " + findTeam);
 
-            em.persist(memberV11);
-            em.persist(memberV12);*/
-
-            //em.flush(); // 트랜잭션 커밋전에 강제반영
-
-            //System.out.println("=======실행확인선============");
-
-            // 트랜잭션 커밋. 커밋하는 순간 flush()를 통해 cud를 실행함.
-            // r은 트랜잭션이 아니어도 되지만 동일성 보장을 위해 트랜색션 안에서 하는게 좋음
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,22 +2,43 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
-import java.util.Date;
-
 @Entity
 public class Member {
     @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
-    @Column(name = "name")
+    @Column(name = "USERNAME")
     private String username;
-    private Integer age;
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-    @Lob
-    private String description;
-    //Getter, Setter…
+    /*@Column(name = "TEAM_ID")
+    private Long teamId;*/
+
+    //회원이 many 팀이 one 외래키가 있는쪽이 many임
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
