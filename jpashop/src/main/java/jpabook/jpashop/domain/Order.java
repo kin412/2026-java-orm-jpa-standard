7f7ff9a49ea1f574953a759f1d8686218bd5a8df
MINDEV\kin41 <kin412@naver.com>
2026-02-21 23:39:20 +0900

    연관관계 매핑 기초 - 실전 예제2 - 연관관계 매핑 시작

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -2,8 +2,11 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 
+import javax.naming.Name;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
@@ -14,12 +17,55 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+    /*@Column(name = "MEMBER_ID")
+    private Long memberId;*/
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<OrderItem>();
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
     private LocalDateTime orderDate;
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 
 }
